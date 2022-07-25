da0dab4d0d330fac26b5f2d5df0ac6609cb4f6b7
Holder19 <Manhunt19@inbox.ru>
2022-07-25 14:52:59 +0300

    Выполняет блок контактов

---
+++ source/js/modules/form.js
@@ -0,0 +1,52 @@
+import IMask from 'imask';
+
+const inputsTel = document.querySelectorAll('[data-input=tel]');
+const inputsName = document.querySelectorAll('[data-input=name]');
+const TEL_LENGTH = 16;
+
+const initMask = (element) => {
+  const maskOptions = {
+    mask: '+{7}(000)000-00-00',
+  };
+  // eslint-disable-next-line new-cap
+  IMask(element, maskOptions);
+};
+
+const addListenerOnTelChange = () => {
+  inputsTel.forEach((inputTel) => {
+
+    initMask(inputTel);
+
+    inputTel.addEventListener('change', () => {
+      if (inputTel.value.length < TEL_LENGTH) {
+        inputTel.setCustomValidity('Пожалуйста введите ваш номер полностью, пример: +7(999)999-99-99');
+      } else {
+        inputTel.setCustomValidity('');
+      }
+      inputTel.reportValidity();
+    });
+  });
+};
+
+const addListenerOnNameChange = () => {
+  inputsName.forEach((inputName)=> {
+
+    inputName.addEventListener('change', () => {
+      const nameValue = inputName.value;
+
+      if (nameValue === '') {
+        inputName.setCustomValidity('Пожалуйста введите ваше имя');
+      } else {
+        inputName.setCustomValidity('');
+      }
+      inputName.reportValidity();
+    });
+  });
+};
+
+const addListenersOnForm = () => {
+  addListenerOnTelChange();
+  addListenerOnNameChange();
+};
+
+export {addListenersOnForm};
